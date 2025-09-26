5fde8549e6c86a0d05184f85b2c66b8fabe4cb95
mansoor <mansoore4@gmail.com>
2025-09-26 11:30:18 +0530

    bug fixes for multi barcode uom in pos

--- kitchenkraft_multiple_barcode/__manifest__.py
+++ kitchenkraft_multiple_barcode/__manifest__.py
@@ -1,14 +1,26 @@
 {
-    'name': 'kitchenkraft Multiple Barcode',
-    'version': '18.2.1',
-    'category': 'Inventory',
-    'summary': 'Multiple Barcode for kitchenkraft',
-    'depends': ['product', 'point_of_sale', 'stock','web'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/multiple_barcode_views.xml',
-        'views/product_template_view.xml',
+    "name": "kitchenkraft Multiple Barcode",
+    "version": "18.2.1",
+    "category": "Inventory",
+    "summary": "Multiple Barcode for kitchenkraft",
+    "license": "OPL-1",
+    "depends": [
+        "product",
+        "point_of_sale",
+        "stock",
+        "web",
     ],
-    'installable': True,
-    'application': True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/multiple_barcode_views.xml",
+        "views/product_template_view.xml",
+    ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "kitchenkraft_multiple_barcode/static/src/js/pos_order_line_patch.js",
+            "kitchenkraft_multiple_barcode/static/src/js/product_screen.js",
+        ],
+    },
+    "installable": True,
+    "application": True,
 }
