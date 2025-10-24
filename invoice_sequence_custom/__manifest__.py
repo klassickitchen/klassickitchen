20eec64df03d3d2666b53d427699145990596239
AlonaSunny <alonasunny295@gmail.com>
2025-10-24 18:17:08 +0530

    invoice change

--- invoice_sequence_custom/__manifest__.py
+++ invoice_sequence_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
 	'name': "Custom invoice sequence number",
-	'version': '18.0.1.0.1',
+	'version': '18.0.3.0.1',
 	'category': 'Point of Sale',
 	'summary': "This module is used to add custom serial number for invoices",
     'author': 'AGM Global Services',
