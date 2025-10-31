a1963bb5577b03090d983231f7f583cd4eb9752d
AlonaSunny <alonasunny295@gmail.com>
2025-10-31 16:25:11 +0530

    barcode alignment

--- kitchenkraft_multiple_barcode/__manifest__.py
+++ kitchenkraft_multiple_barcode/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "kitchenkraft Multiple Barcode",
-    "version": "18.10.1",
+    "version": "18.11.1",
     "category": "Inventory",
     "summary": "Multiple Barcode for kitchenkraft",
     "license": "OPL-1",
