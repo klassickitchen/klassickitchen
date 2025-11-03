530556a955e54ca9862f85d291d3e956f05273b4
Sakthivelkvs <sakthivelkvs4@gmail.com>
2025-11-03 12:10:39 +0530

    header and footer module added

---
+++ kitchenkraft_header_and_footer/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Header and Footer for KitchenKraft",
+    'version': '18.1.1',
+    'author': 'AGM Global Services',
+    'description': """Header and Footer for KitchenKraft""",
+    'depends': ['sale_management', 'sale', 'stock', 'web', 'base', 'account'],
+    'data': [
+        'reports/report_template.xml',
+        'views/res_company_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+
+}
