b2095532f1c6dad33215f4f1ed4e7470035de6a2
Sakthivelkvs <sakthivelkvs4@gmail.com>
2025-11-05 12:35:37 +0530

    font style chnaged for a5 and barcode alignment fixed

--- kitchenkraft_multiple_barcode/__manifest__.py
+++ kitchenkraft_multiple_barcode/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "kitchenkraft Multiple Barcode",
-    "version": "18.11.2",
+    "version": "18.11.3",
     "category": "Inventory",
     "summary": "Multiple Barcode for kitchenkraft",
     "license": "OPL-1",
--- kitchenkraft_report_custom_font/__manifest__.py
+++ kitchenkraft_report_custom_font/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Kitchen Kraft Report Custom Font',
-    'version': '1.0',
+    'version': '1.1',
     'summary': 'Applies a custom font to all PDF reports',
     'description': """
         This module applies a custom dot matrix-style font globally across
