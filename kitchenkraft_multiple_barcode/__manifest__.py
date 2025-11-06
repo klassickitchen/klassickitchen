799f432c015e9424896e2823bc62176de0c1853f
Sakthivelkvs <sakthivelkvs4@gmail.com>
2025-11-06 12:48:42 +0530

    font size adjusted in a5 and barcode adjustment

--- kitchenkraft_multiple_barcode/__manifest__.py
+++ kitchenkraft_multiple_barcode/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "kitchenkraft Multiple Barcode",
-    "version": "18.11.4",
+    "version": "18.11.5",
     "category": "Inventory",
     "summary": "Multiple Barcode for kitchenkraft",
     "license": "OPL-1",
