4ca5bc6fb25a0df9316b0182d9f0dc748884e0e6
Sakthivelkvs <sakthivelkvs4@gmail.com>
2025-11-12 15:18:54 +0530

    updated font

--- kitchenkraft_multiple_barcode/__manifest__.py
+++ kitchenkraft_multiple_barcode/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "kitchenkraft Multiple Barcode",
-    "version": "18.12.2",
+    "version": "18.12.3",
     "category": "Inventory",
     "summary": "Multiple Barcode for kitchenkraft",
     "license": "OPL-1",
