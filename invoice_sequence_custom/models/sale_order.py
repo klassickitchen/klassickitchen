f1fe9cabf808d30a1c9f020c43d2693fdc399f6c
mansoor <mansoore4@gmail.com>
2025-11-14 18:33:01 +0530

    test

--- invoice_sequence_custom/models/sale_order.py
+++ invoice_sequence_custom/models/sale_order.py
@@ -4,9 +4,9 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    payment_type = fields.Selection(
-        [('cash', 'Cash'),
-         ('credit', 'Credit'),
-         ], string='Cash|Credit',
-        required=True
-    )
+    # payment_type = fields.Selection(
+    #     [('cash', 'Cash'),
+    #      ('credit', 'Credit'),
+    #      ], string='Cash|Credit',
+    #     required=True
+    # )
