d2727c337e66f9313eb1dc04129a2a79ace72dac
mansoor <mansoore4@gmail.com>
2025-11-14 18:36:38 +0530

    test

--- invoice_sequence_custom/models/account_move.py
+++ invoice_sequence_custom/models/account_move.py
@@ -4,25 +4,25 @@ from odoo import models, api, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
     is_pos_invoice = fields.Boolean(string="POS Invoice", default=False)
-    payment_type = fields.Selection(
-        [('cash', 'Cash'),
-         ('credit', 'Credit')],
-        string='Cash|Credit',
-        compute="_compute_payment_type"
-    )
-
-    @api.depends('invoice_line_ids.sale_line_ids.order_id.payment_type')
-    def _compute_payment_type(self):
-        for move in self:
-            if move.is_pos_invoice:
-                move.payment_type = 'cash'
-                continue
-
-            sale_orders = move.invoice_line_ids.sale_line_ids.mapped('order_id')
-            if sale_orders:
-                move.payment_type = sale_orders[0].payment_type
-            else:
-                move.payment_type = False
+    # payment_type = fields.Selection(
+    #     [('cash', 'Cash'),
+    #      ('credit', 'Credit')],
+    #     string='Cash|Credit',
+    #     compute="_compute_payment_type"
+    # )
+    #
+    # @api.depends('invoice_line_ids.sale_line_ids.order_id.payment_type')
+    # def _compute_payment_type(self):
+    #     for move in self:
+    #         if move.is_pos_invoice:
+    #             move.payment_type = 'cash'
+    #             continue
+    #
+    #         sale_orders = move.invoice_line_ids.sale_line_ids.mapped('order_id')
+    #         if sale_orders:
+    #             move.payment_type = sale_orders[0].payment_type
+    #         else:
+    #             move.payment_type = False
 
     # @api.model
     # def create(self, vals_list):
