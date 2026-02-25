710b7a56854d0bf9b36abf624a096a9704bb8e62
mansoor <mansoore4@gmail.com>
2026-02-25 14:13:49 +0530

    discount updates

---
+++ sh_pos_show_discount/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "POS Global Discount Auto Calculation",
+    "version": "18.0.1.0.0",
+    "author": "AGM Info Solutions",
+    "website": "https://www.agmglobal.in/",
+    "category": "Point Of Sale",
+    "summary": "Auto calculate equivalent Fixed/Percentage discount in POS Global Discount popup",
+    "description": """
+        When the user enters a Fixed Discount value, the system automatically calculates
+        and displays the equivalent Percentage Discount, and vice versa.
+    """,
+    "depends": ["sh_pos_order_discount"],
+    "application": False,
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "sh_pos_show_discount/static/src/**/*",
+        ],
+    },
+    "auto_install": False,
+    "installable": True,
+}
