54f26c10f77f08c90e583f12019377975f31445d
athulya116 <athulyaathu9562@gmail.com>
2026-03-03 12:00:17 +0530

    changes

--- amount_in_words_invoice/__manifest__.py
+++ amount_in_words_invoice/__manifest__.py
@@ -22,7 +22,7 @@
 ###############################################################################
 {
     'name': "Amount In Words In Invoice, Sale Order And Purchase Order",
-    'version': '18.0.1.3.0',
+    'version': '18.0.1.3.1',
     'category': 'Accounting',
     'summary': """Showing the subtotal amounts of invoice, sale order 
      and purchase order in words""",
