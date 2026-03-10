cfc29c3b0656dfcc3e34cde260011efb4fc9dd43
nishumusthafa4736-max <nishumusthafa4736@gmail.com>
2026-03-10 08:45:11 +0530

    Changes - Invoice Report Changes and Layout Update

--- kitchenkraft_header_and_footer/__manifest__.py
+++ kitchenkraft_header_and_footer/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Header and Footer for KitchenKraft",
-    'version': '18.1.7',
+    'version': '18.1.8',
     'author': 'AGM Global Services',
     'description': """Header and Footer for KitchenKraft""",
     'depends': ['sale_management', 'sale', 'stock', 'web', 'base', 'account'],
