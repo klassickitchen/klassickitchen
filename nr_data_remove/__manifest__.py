ec9c0ffd889e2973cc40ef0a5e07e6a8c4803c0b
rishankv <rishankvmadayi@gmail.com>
2026-03-10 15:04:20 +0530

    complete_data_clear_and_removal,nr_data_remove

---
+++ nr_data_remove/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Odoo - Data Remove Odoo Online, Odoo.sh, On Premise',
+    'version': '1.0',
+    'author': 'Nirav Rathod',
+    'category': 'Tools',
+    'summary': 'To remove various module data. (Odoo Online, Odoo.sh, On Premise)',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': ['data/remove_data_actions.xml', 'views/remove_data_settings.xml'],
+    'installable': True,
+    'application': False,
+    'images': ['static/description/banner.png'],
+}
