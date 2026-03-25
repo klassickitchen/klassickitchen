ad51a4dc7c079e2e86ac95456c10a9f585dca749
rishankv <rishankvmadayi@gmail.com>
2026-03-25 11:12:53 +0530

    sh_pos_show_discount (18.0.1.0.2): Added product quantity, item quantity, and order quantity details in POS.

---
+++ sh_pos_show_discount/static/src/overrides/components/order_widget/order_widget_ext.js
@@ -0,0 +1,26 @@
+/** @odoo-module */
+import { patch } from "@web/core/utils/patch";
+import { OrderWidget } from "@point_of_sale/app/generic_components/order_widget/order_widget";
+
+patch(OrderWidget.prototype, {
+    get_product_qty() {
+        var order = this.pos.get_order();
+        if (!order) return 0;
+        return order.get_orderlines().length;
+    },
+    get_zero_qty() {
+        var order = this.pos.get_order();
+        if (!order) return 0;
+        return order.get_orderlines().filter(line => line.qty === 0).length;
+    },
+    get_items_qty() {
+        var order = this.pos.get_order();
+        if (!order) return 0;
+        var orderlines = order.get_orderlines();
+        var total = 0;
+        for (let i = 0; i < orderlines.length; i++) {
+            total += orderlines[i].qty;
+        }
+        return total;
+    }
+});
