3f7b55de222b70a1fe826dcfd412bc16ed72d302
HibaParveen <hibaparveen032@gmail.com>
2026-03-27 18:16:08 +0530

    Below points added to klassickitchen staging: 1. invoice_sequence_custom(version: 18.0.3.10.6)-> Add sequence for invoice return and pos return

--- invoice_sequence_custom/models/pos_order.py
+++ invoice_sequence_custom/models/pos_order.py
@@ -1,5 +1,6 @@
 from odoo import models
 
+
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
@@ -8,9 +9,37 @@ class PosOrder(models.Model):
         if invoice:
             company_code = self.company_id.code or ''
             invoice.is_pos_invoice = True
-            # Assign POS-specific sequence if not yet assigned
-            if not invoice.name or invoice.name == '/':
-                seq = self.env['ir.sequence'].next_by_code('pos.invoice')
+
+            if invoice.move_type == 'out_refund':
+                # Find sequence for THIS company specifically
+                sequence = self.env['ir.sequence'].search([
+                    ('code', '=', 'pos.invoice.return'),
+                    ('company_id', '=', self.company_id.id)
+                ], limit=1)
+
+                # If not found for this company, create it automatically
+                if not sequence:
+                    sequence = self.env['ir.sequence'].sudo().create({
+                        'name': 'POS Invoice Return',
+                        'code': 'pos.invoice.return',
+                        'prefix': 'POS RET/',
+                        'padding': 4,
+                        'company_id': self.company_id.id,
+                    })
+
+                seq = sequence.next_by_id()
                 if seq:
-                    invoice.name = f"{company_code}/{seq}"
+                    self.env.cr.execute(
+                        "UPDATE account_move SET name = %s WHERE id = %s",
+                        (f"{company_code}/{seq}", invoice.id)
+                    )
+                    invoice.invalidate_recordset(['name'])
+
+
+            elif invoice.move_type == 'out_invoice':
+                if not invoice.name or invoice.name == '/':
+                    seq = self.env['ir.sequence'].next_by_code('pos.invoice')
+                    if seq:
+                        invoice.name = f"{company_code}/{seq}"
+
         return invoice
