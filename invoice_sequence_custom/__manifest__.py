9d57828b131c1b6e22a4c9e210fad87b4bcfd8d3
rishankv <rishankvmadayi@gmail.com>
2026-04-20 10:52:30 +0530

    invoice_sequence_custom(18.0.4.1.9) Credit Note size issue solved

--- invoice_sequence_custom/__manifest__.py
+++ invoice_sequence_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Custom invoice sequence number",
-    "version": "18.0.4.1.8",
+    "version": "18.0.4.1.9",
     "category": "Point of Sale",
     "summary": "This module is used to add custom serial number for invoices",
     "author": "AGM Global Services",
