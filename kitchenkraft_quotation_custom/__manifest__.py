ea76230dbd781ed15542e1b6cd2b0dfbd1989e44
nishumusthafa4736-max <nishumusthafa4736@gmail.com>
2026-05-04 15:19:12 +0530

    Discount Boolean Field Added to Product Template, Report will not include the discription of such Product

--- kitchenkraft_quotation_custom/__manifest__.py
+++ kitchenkraft_quotation_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Kitchen Kraft Quotation Custom',
-    'version': '18.0.1.0',
+    'version': '18.0.1.1',
     'description': 'Kitchen Kraft Quotation',
     'author': 'AGM Global Services',
     'website': 'http://agmglobal.co',
@@ -8,6 +8,7 @@
     'data': [
         'views/sale_order_line.xml',
         'views/stock_move.xml',
+        'views/product_template_views.xml',
         'reports/quotation_report.xml',
     ],
     'installable': True,
