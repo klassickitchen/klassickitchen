75a4d84f25acf228b6f5060b3dacc17b1088c38f
nishumusthafa4736-max <nishumusthafa4736@gmail.com>
2026-05-11 10:54:42 +0530

    Invoice Remove Restriction Default True

--- invoice_sequence_custom/models/account_move.py
+++ invoice_sequence_custom/models/account_move.py
@@ -20,7 +20,7 @@ class AccountMove(models.Model):
         string="Total Discount", currency_field='currency_id', compute='_custom_compute_amount'
     )
 
-    use_custom_description = fields.Boolean(string='Remove Description', default=False)
+    use_custom_description = fields.Boolean(string='Remove Description', default=True)
 
     @api.depends('invoice_line_ids')
     def _custom_compute_amount(self):
