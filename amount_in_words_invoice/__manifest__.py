43391e6c5634fba6ed70109e0c9cb9ec430fbf41
nishumusthafa4736-max <nishumusthafa4736@gmail.com>
2026-05-11 10:35:31 +0530

    Page Break inside Amount in words of Invoice Restricted

--- amount_in_words_invoice/__manifest__.py
+++ amount_in_words_invoice/__manifest__.py
@@ -22,7 +22,7 @@
 ###############################################################################
 {
     'name': "Amount In Words In Invoice, Sale Order And Purchase Order",
-    'version': '18.0.1.3.3',
+    'version': '18.0.1.3.4',
     'category': 'Accounting',
     'summary': """Showing the subtotal amounts of invoice, sale order 
      and purchase order in words""",
