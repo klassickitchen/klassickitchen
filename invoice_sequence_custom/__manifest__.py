e88d3d9e0df4a2616a3d0d47ce09826307f6a0b5
Ebin_seb <ebinsebastianthekkanattu@gmail.com>
2026-08-23 10:54:22 +0530

    invoie title changes on A5 paperformat

--- invoice_sequence_custom/__manifest__.py
+++ invoice_sequence_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Custom invoice sequence number",
-    "version": "18.0.4.2.5",
+    "version": "18.0.4.2.6",
     "category": "Point of Sale",
     "summary": "This module is used to add custom serial number for invoices",
     "author": "AGM Global Services",
