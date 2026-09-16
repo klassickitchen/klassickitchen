85577da98f874eb45a6cfdeef161a3ce89417ec6
Ebin_seb <ebinsebastianthekkanattu@gmail.com>
2026-09-16 23:28:52 +0530

    invoice changes in designs

--- amount_in_words_invoice/__manifest__.py
+++ amount_in_words_invoice/__manifest__.py
@@ -22,7 +22,7 @@
 ###############################################################################
 {
     'name': "Amount In Words In Invoice, Sale Order And Purchase Order",
-    'version': '18.0.1.3.4',
+    'version': '18.0.1.3.5',
     'category': 'Accounting',
     'summary': """Showing the subtotal amounts of invoice, sale order 
      and purchase order in words""",
